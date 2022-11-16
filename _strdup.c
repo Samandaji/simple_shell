cf51b1af3797f511f4caf8467ae286c24c0ecac7
samandaji <contactabubakarusman@gmail.com>
2022-11-16 20:34:46 +0100

    added _strdup files

---
+++ _strdup.c
@@ -0,0 +1,31 @@
+#include "_shell.h"
+
+/**
+ * _strdup - duplicates a string
+ * @s: given str
+ * Return: pointer to duplicate
+ */
+
+char *_strdup(char *s)
+{
+	char *str;
+	int len = 0, i;
+
+	if (!s)
+		return (NULL);
+
+	while (s[len] != '\0')
+		len++;
+
+	str = malloc(sizeof(char) * len);
+	
+	if (!str)
+		return (NULL);
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		str[i] = s[i];
+	}
+	str[i] = '\0';
+	return (str);
+}
